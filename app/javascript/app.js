7da6ff750ef3599fcc38aaa8e0b59cfa1878e8c0
Blaine Arnau <blainearnau@gmail.com>
2016-08-10 16:27:10 -0700

    Add multiple list functionality

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -2,55 +2,68 @@ var app = angular.module('app', ['as.sortable'])
 
 .controller('mainCtrl', function($scope) {
 
-	$scope.toDos = [];
-	$scope.reverse = true;
+    $scope.lists = [];
 
-	
-	//handle submit click and add single todo to toDos array
-	$scope.createToDo = function() {
 
-		//assign a priority number to toDo based on value from select box
-		var priorityNumber = 0;
-		switch ($scope.priority) {
-			case "list-group-item-danger":
-				priorityNumber = 1;
-				break;
-			case "list-group-item-warning":
-				priorityNumber = 2;
-				break;
-			case "list-group-item-success":
-				priorityNumber = 3;
-				break;
-			default: 
-				priorityNumber = 1;
 
-		}
 
-		//push item to array
-		$scope.toDos.push({
-			'description': $scope.text,
-			'priorityClass': $scope.priority,
-			'priorityNumber': priorityNumber
-		});
+    $scope.reverse = true;
 
-		//set values of inputs back to zero
-		$scope.text = '';
-		$scope.priority = 0;
-	}
 
-	//remove toDo from list 
-	$scope.removeToDo = function(toDo) {
-		var i = $scope.toDos.indexOf(toDo);
-		$scope.toDos.splice(i, 1);
-		
+    //handle submit click and add single todo to toDos array
+    $scope.createToDo = function(list) {
 
-	}
+        //assign a priority number to toDo based on value from select box
+        var priorityNumber = 0;
+        switch (list.newPriority) {
+            case "list-group-item-danger":
+                priorityNumber = 1;
+                break;
+            case "list-group-item-warning":
+                priorityNumber = 2;
+                break;
+            case "list-group-item-success":
+                priorityNumber = 3;
+                break;
+            default:
+                priorityNumber = 1;
 
-	//part of ng-sortable
-	// $scope.$watch('toDos', function () {
-	//     console.log(arguments);
-	//   });
-	// $scope.sortableOptions = {
-	//     containment: '#sortable-container'
-	//   };
+        }
+
+        //push item to array
+        list.toDos.push({
+            'description': list.newText,
+            'priorityClass': list.newPriority,
+            'priorityNumber': priorityNumber
+        });
+
+
+
+
+
+        
+
+        //set values of inputs back to zero
+
+        $scope.text = '';
+        $scope.priority = 0;
+    }
+
+    //remove toDo from list 
+    $scope.removeToDo = function(list, toDo) {
+        var i = list.toDos.indexOf(toDo);
+        list.toDos.splice(i, 1);
+    }
+
+    $scope.newList = function() {
+        if ($scope.listName !== undefined) {
+
+            $scope.lists.push({
+                name: $scope.listName,
+                toDos: []
+            });
+            $("input[ng-model='listName']").val('');
+            $scope.listName = undefined;
+        }
+    }
 });
